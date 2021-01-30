095a008d2feb88ee93ce49047b424b9eb8bbf37b
Yash Sinha <yashsinha4909@gmail.com>
2021-01-30 22:58:24 +0530

    changes in segT file

--- tests/test_myfunctions.py
+++ tests/test_myfunctions.py
@@ -1,7 +1,7 @@
-from segT import myfunctions
+from segT import oper
 def test_build():
     arr = [1,2,3,4,5,6,7]
-    segt = myfunctions.build(arr, 'sum')
-    print(myfunctions.query(arr, 2, 4, segt, 'sum'))
-    myfunctions.update(arr, 2, 6, segt, 'sum')
-    print(myfunctions.query(arr, 2, 4, segt, 'sum'))
+    segt = oper.build(arr, 'sum')
+    print(oper.query(arr, 2, 4, segt, 'sum'))
+    oper.update(arr, 2, 6, segt, 'sum')
+    print(oper.query(arr, 2, 4, segt, 'sum'))
